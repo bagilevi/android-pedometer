ca291e4cd7784de9fab3922ba11802ab791c903c
unknown <Levente@.(none)>
2009-05-03 18:17:34 +0200

    alternative displays

--- src/name/bagi/levente/pedometer/R.java
+++ src/name/bagi/levente/pedometer/R.java
@@ -14,41 +14,52 @@ public final class R {
     }
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int button=0x7f060005;
+        public static final int button_sign=0x7f060006;
+        public static final int desired_pace=0x7f060003;
+        public static final int desired_pace_width=0x7f060004;
+        public static final int distance=0x7f060008;
+        public static final int large_half=0x7f060000;
+        public static final int margin=0x7f060007;
+        public static final int pace=0x7f060002;
+        public static final int step_count=0x7f060001;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button_desired_pace_lower=0x7f070005;
-        public static final int button_desired_pace_raise=0x7f070008;
-        public static final int desired_pace_control=0x7f070004;
-        public static final int desired_pace_label=0x7f070006;
-        public static final int desired_pace_value=0x7f070007;
-        public static final int pace_units=0x7f070003;
-        public static final int pace_value=0x7f070002;
-        public static final int step_count=0x7f070000;
-        public static final int step_label=0x7f070001;
+        public static final int button_desired_pace_lower=0x7f080005;
+        public static final int button_desired_pace_raise=0x7f080008;
+        public static final int desired_pace_control=0x7f080004;
+        public static final int desired_pace_label=0x7f080006;
+        public static final int desired_pace_value=0x7f080007;
+        public static final int pace_units=0x7f080003;
+        public static final int pace_value=0x7f080002;
+        public static final int step_count=0x7f080000;
+        public static final int step_label=0x7f080001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int activity_settings=0x7f060005;
-        public static final int app_name=0x7f060001;
-        public static final int desired_pace=0x7f060004;
-        public static final int desired_pace_setting=0x7f06000d;
-        public static final int desired_pace_setting_details=0x7f06000e;
-        public static final int desired_pace_voice_setting=0x7f06000f;
-        public static final int desired_pace_voice_setting_details=0x7f060010;
-        public static final int hello=0x7f060000;
-        public static final int pace_setting=0x7f06000b;
-        public static final int pace_setting_details=0x7f06000c;
-        public static final int pace_settings_title=0x7f06000a;
-        public static final int pace_units=0x7f060003;
-        public static final int sensitivity_setting=0x7f060007;
-        public static final int sensitivity_setting_details=0x7f060008;
-        public static final int sensitivity_setting_title=0x7f060009;
-        public static final int steps=0x7f060002;
-        public static final int steps_settings_title=0x7f060006;
+        public static final int activity_settings=0x7f070005;
+        public static final int app_name=0x7f070001;
+        public static final int desired_pace=0x7f070004;
+        public static final int desired_pace_setting=0x7f07000d;
+        public static final int desired_pace_setting_details=0x7f07000e;
+        public static final int desired_pace_voice_setting=0x7f07000f;
+        public static final int desired_pace_voice_setting_details=0x7f070010;
+        public static final int hello=0x7f070000;
+        public static final int pace_setting=0x7f07000b;
+        public static final int pace_setting_details=0x7f07000c;
+        public static final int pace_settings_title=0x7f07000a;
+        public static final int pace_units=0x7f070003;
+        public static final int sensitivity_setting=0x7f070007;
+        public static final int sensitivity_setting_details=0x7f070008;
+        public static final int sensitivity_setting_title=0x7f070009;
+        public static final int steps=0x7f070002;
+        public static final int steps_settings_title=0x7f070006;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
