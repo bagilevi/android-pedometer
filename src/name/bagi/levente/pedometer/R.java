dc0771679e450e9b6435982c0777b781df04b400
unknown <Levente@.(none)>
2009-05-16 20:49:56 +0200

    cleanup

--- src/name/bagi/levente/pedometer/R.java
+++ src/name/bagi/levente/pedometer/R.java
@@ -44,27 +44,26 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int activity_settings=0x7f070005;
-        public static final int app_name=0x7f070001;
-        public static final int desired_pace=0x7f070004;
-        public static final int desired_pace_setting=0x7f07000d;
-        public static final int desired_pace_setting_details=0x7f07000e;
-        public static final int desired_pace_voice_setting=0x7f07000f;
-        public static final int desired_pace_voice_setting_details=0x7f070010;
-        public static final int hello=0x7f070000;
-        public static final int notification_subtitle=0x7f070011;
-        public static final int pace_setting=0x7f07000b;
-        public static final int pace_setting_details=0x7f07000c;
-        public static final int pace_settings_title=0x7f07000a;
-        public static final int pace_units=0x7f070003;
-        public static final int quit=0x7f070014;
-        public static final int sensitivity_setting=0x7f070007;
-        public static final int sensitivity_setting_details=0x7f070008;
-        public static final int sensitivity_setting_title=0x7f070009;
-        public static final int settings=0x7f070013;
-        public static final int steps=0x7f070002;
-        public static final int steps_settings_title=0x7f070006;
-        public static final int stopped=0x7f070012;
+        public static final int activity_settings=0x7f070004;
+        public static final int app_name=0x7f070000;
+        public static final int desired_pace=0x7f070003;
+        public static final int desired_pace_setting=0x7f07000c;
+        public static final int desired_pace_setting_details=0x7f07000d;
+        public static final int desired_pace_voice_setting=0x7f07000e;
+        public static final int desired_pace_voice_setting_details=0x7f07000f;
+        public static final int notification_subtitle=0x7f070010;
+        public static final int pace_setting=0x7f07000a;
+        public static final int pace_setting_details=0x7f07000b;
+        public static final int pace_settings_title=0x7f070009;
+        public static final int pace_units=0x7f070002;
+        public static final int quit=0x7f070013;
+        public static final int sensitivity_setting=0x7f070006;
+        public static final int sensitivity_setting_details=0x7f070007;
+        public static final int sensitivity_setting_title=0x7f070008;
+        public static final int settings=0x7f070012;
+        public static final int steps=0x7f070001;
+        public static final int steps_settings_title=0x7f070005;
+        public static final int stopped=0x7f070011;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
